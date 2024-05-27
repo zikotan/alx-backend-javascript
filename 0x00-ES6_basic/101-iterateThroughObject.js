b24c185e7d973ad93df01ef8a089f3c5b723950c
zikotan <tananzakaria630@gmail.com>
2024-05-27 22:26:23 +0100

    response

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,14 +1,6 @@
-export default function createReportObject(employeesList) {
-  const allEmployees = {};
-
-  for (const [departmentName, employees] of Object.entries(employeesList)) {
-    allEmployees[departmentName] = [...employees];
-  }
-
-  return {
-    allEmployees,
-    getNumberOfDepartments() {
-      return Object.keys(employeesList).length;
-    },
-  };
+//  function named iterateThroughObject. The function’s parameter
+// reportWithIterator is the return value from createIteratorObject.
+// return every employee name in a string, separated by |
+export default function iterateThroughObject(reportWithIterator) {
+  return reportWithIterator.join(' | ');
 }
