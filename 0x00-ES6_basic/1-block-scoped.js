9e0664c3b1e6639200f5bae7531537e9440df1d4
zikotan <tananzakaria630@gmail.com>
2024-05-27 22:16:17 +0100

    response

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    var task = true;
-    var task2 = false;
+    task = true;
+    task2 = false;
   }
 
   return [task, task2];
